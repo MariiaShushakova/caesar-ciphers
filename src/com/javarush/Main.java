37ad0dac0bba89ab73c06ba87eb75b03d9525306
mariia <mariia.shushakova@gmail.com>
2022-12-15 20:19:07 +0200

    encrypter + decrypter

--- src/com/javarush/Main.java
+++ src/com/javarush/Main.java
@@ -1,8 +1,57 @@
 package com.javarush;
 
+import java.util.Scanner;
+import static com.javarush.Symbols.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type line:");
+        String line = sc.nextLine();
+
+        System.out.println("Type shift key (int):");
+        int shift = sc.nextInt();
+
+        String encrypted = encrypt(line, shift);
+        System.out.println(encrypted);
+        System.out.println(decrypt(encrypted, shift));
+    }
+
+    public static String  encrypt(String line, int shift){
+        //System.out.println("Initial line: " + line);
+        StringBuilder initialStr = new StringBuilder(line.toLowerCase()); //TODO: need to think
+        StringBuilder encryptedStr = new StringBuilder();
+        
+        for (int i = 0; i < initialStr.length(); i++) {
+            var ch  = initialStr.charAt(i);
+
+            if(STR_CRYP.indexOf(ch) >= 0) { //check existence in our alphabet
+                encryptedStr.append(STR_CRYP.charAt((STR_CRYP.indexOf(ch) + shift) % STR_CRYP.length()));
+            }else{
+                encryptedStr.append(ch);
+            }
+        }
+        return String.valueOf(encryptedStr);
+    }
+
+    public static String decrypt(String line, int shift){
+        //System.out.println("Initial line: " + line);
+        StringBuilder initialStr = new StringBuilder(line.toLowerCase()); //TODO: need to think
+        StringBuilder decryptedStr = new StringBuilder();
+
+        for (int i = 0; i < initialStr.length(); i++) {
+            var ch  = initialStr.charAt(i);
+
+            if(STR_CRYP.indexOf(ch) >= 0) {
+
+                int indexDecr = (STR_CRYP.indexOf(ch) + STR_CRYP.length() - shift % STR_CRYP.length()) % STR_CRYP.length();
+                decryptedStr.append(STR_CRYP.charAt(indexDecr));
+            }else{
+                decryptedStr.append(ch);
+            }
+        }
+        return String.valueOf(decryptedStr);
     }
 }
---
+++ src/com/javarush/Symbols.java
@@ -0,0 +1,10 @@
+package com.javarush;
+
+public class Symbols {
+
+    public  static final String STR_CRYP = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,:-!? ";
+    public  static final String STR_SYMBOLS = ".,:-!? ";
+    public  static final String VOWELS = "аеёиоуыэюя";
+    public  static final String CONSONANTS = "бвгджзйклмнпрстфхцчшщъь";
+
+}
